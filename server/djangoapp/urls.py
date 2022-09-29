e5f663adfb515ccbd022dded555d1230d5cc6a5b
byordanova <borislava.yordanova8@gmail.com>
2022-09-29 06:34:50 -0400

    Initial

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,26 +5,20 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
     path(route='', view=views.get_dealerships, name='index'),
-    path(route='djangoapp/about', view=views.about, name='about'),
-    path(route='djangoapp/contact', view=views.contact, name='contact')
 
-    # path for dealer reviews view
+    path(route='about', view=views.about, name='about'),
 
-    # path for add a review view
+    path(route='contact', view=views.contact, name='contact'),
+
+    path('registration/', views.registration_request, name='registration'),
+
+    path('login/', views.login_request, name='login'),
+
+    path('logout/', views.logout_request, name='logout'),
+
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+
+    path('review/<int:id>/', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
